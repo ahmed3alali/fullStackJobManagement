7aab6c7762cb25e647ba95bee84466e33db86be4
ahmed3alali <ahmed3alali.development@gmail.com>
2024-11-14 22:48:42 +0300

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
         element: <Landing />,
       },
       {
-        path: 'register',
+        path: '/register',
         element: <Register />,
         action: registerAction,
       },
