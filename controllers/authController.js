0ded6463ed796fa2f0bc70a0fb9555003b3391b7
ahmed3alali <ahmed3alali.development@gmail.com>
2025-01-25 00:25:50 +0300

    login adjustments

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,80 +1,64 @@
+
 import { StatusCodes } from "http-status-codes";
+
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
+
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
-import { comparePassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
 import { createJWT } from "../utils/tokenUtils.js";
 
-export const register = async (req, res) => {
-    try {
-        const isFirstAccount = await User.countDocuments() === 0;
-        req.body.role = isFirstAccount ? 'admin' : 'us';
 
-        // Hash the passwordx by setting unvieed   
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword;
 
-        const user = await User.create(req.body);
-        res.status(StatusCodes.CREATED).json({ msg: 'User created', userId: user._id });
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Error creating user', error: error.message });
-    }
+
+
+export const register = async(req,res) => {
+const isFirstAccount =await User.countDocuments()===0
+
+req.body.role = isFirstAccount?'admin':'user';
+
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(req.body.password,salt);
+req.body.password = hashedPassword;
+const user = await User.create(req.body);
+res.status(StatusCodes.CREATED).json({msg: 'user created'});
+
+
+res.send('register');
+
+
 };
 
 
 
 
-export const login = async (req, res) => {
-    try {
 
-const user = await.
+export const login = async(req,res) =>{
 
-        // Find the user by email
-        const user = await User.findOne({ email: req.body.email });
+const user = await User.findOne({email:req.body.email})
 
-        if (!user) throw new UnauthenticatedError('Invalid user');
+if (!user) throw new UnauthenticatedError('invalid user')
 
-        // Compare the provided password with the stored hashed password
-        const isPasswordCorrect = await comparePassword(req.body.password, user.password);
-        if (!isPasswordCorrect) throw new UnauthenticatedError('Wrong password!');
-
-        // Create a token
-        const token = createJWT({ userId: user._id, role: user.role });
-
-        // Set the token in a cookie
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Set to true in production for secure cookies
-            maxAge: 1000 * 60 * 60 * 24, // Cookie expiration time (1 day)
-        });
-
-        res.status(StatusCodes.OK).json({ msg: 'User logged in', userId: user._id });
-    } catch (error) {
-        if (error instanceof UnauthenticatedError) {
-            res.status(StatusCodes.UNAUTHORIZED).json({ msg: error.message });
-        } else {
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Error logging in', error: error.message });
-        }
-    }
-};
+const isPasswordCorrect = await comparePassword(req.body.password,user.password)
 
 
+if(!isPasswordCorrect) throw new UnauthenticatedError('wrong password!')
 
-export const logout= (req,res)=>  {
+    
 
-    res.cookie('token','logout', {
+
+const oneDay = 1000*60*60*24;
+
+const token = createJWT({userId:user._id, role: user.role});
+res.cookie('token',token,{
 
 
 httpOnly:true,
-expires:new Date(Date.now()),
+expires: new Date(Date.now()+oneDay),
+secure: process.env.NODE_ENV==='production'
 
 
+})
+res.status(StatusCodes.OK).json({msg:'user logged in '})
 
-    });
-
-    res.status(StatusCodes.OK).json({msg:"User logged out!"});
-
-
-
-
-}
+};
